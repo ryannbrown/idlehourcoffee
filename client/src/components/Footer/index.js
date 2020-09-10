a7f239709477cb09939279650694929d5397d9ed
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2020-09-10 06:22:38 -0400

    coming soon page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,13 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import HomeHero from "../../components/HomeHero/index"
 import './style.css';
-// import whiteComet from '../../media/white-comet.png'
+import dots from '../../media/dots.png'
+import clock from '../../media/clock-icon-white.png'
+import cup from '../../media/cup-icon-white.png'
+import flag from '../../media/flag-icon-white.png'
+import latte from '../../media/latte-art-icon-white.png'
+import star from '../../media/star-icon-white.png'
+import tamp from '../../media/tamp-icon-white.png'
 // import starBackground from '../../media/star-background.png'
 
 
@@ -16,22 +22,38 @@ export default class Footer extends Component {
 
         this.listener = null;
         this.state = {
-            status: "top"
+            status: "top",
+            icons: []
         };
     }
 
 
     componentDidMount() {
 
+const icons = [
+    {icon: star},
+    {icon: cup},
+    {icon: clock},
+    {icon: tamp},
+    {icon: latte},
+    {icon: flag}
+];
+
+this.setState({
+    icons: icons
+})
+
     }
 
-    // componentDidUpdate() {
-    //   document.removeEventListener("scroll", this.listener);
-    // }
+
 
     render() {
-
-
+console.log(this.state.icons)
+        if (this.state.icons) {
+            var showIcons = this.state.icons.map((item, i) => (
+                  <img width='100px' src={item.icon}></img>
+              ));
+        }
 
 
 
@@ -51,12 +73,20 @@ export default class Footer extends Component {
             alignItems: `center`,
             position: `relative`,
             width: `350px`,
-            height:`200px`,
+            height:`37.5vh`,
             //   cover no-repeat center center fixed`
         }} className="footer-div">
+            <img className="dots-img" src={dots}></img>
                 <div className="footer-content-box">
-                {/* <h2 className="libre"><i>Blue Heron Pasta Water</i></h2> */}
-            <p className="mt-5">Powered by The Media Team</p>
+                    <div className="icon-box">
+                    {showIcons}
+                    </div>
+                    <p>Wanna give us a call?</p>
+                    <a href="/contact">
+            <button className="contact-btn-link">984-200-1605</button>
+          </a>
+             
+            <p className="justsoup">Powered by Just Soup</p>
             {/* <Image className="comet-image" src={whiteComet}></Image> */}
                 </div>
     
--- client/src/components/HomeHero/index.js
+++ client/src/components/HomeHero/index.js
@@ -1,32 +1,30 @@
-import { Container, Nav, Button, Image, Row, Col } from 'react-bootstrap'
-import Navbar from 'react-bootstrap/Navbar'
-import React, { Component } from 'react';
+import { Container, Nav, Button, Image, Row, Col } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import React, { Component } from "react";
 // import logo from './logo.svg';
 // import HomeHero from "../../components/HomeHero/index"
-import './style.css';
+import "./style.css";
 // import profPic from "../../media/5.PNG"
-import logo from "../../media/logo.png"
-import heroImg from "../../media/background.jpg"
+import logo from "../../media/ihlogo.png";
+import heroImg from "../../media/background.jpg";
+import clock from "../../media/blackclock.png";
+import flag from "../../media/blackflag.png";
 // import arrow from "../../media/arrow.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft, faGlassMartini } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft, faGlassMartini } from "@fortawesome/free-solid-svg-icons";
 
 export default class HomeHero extends Component {
-
   constructor(props) {
     super(props);
 
     this.listener = null;
     this.state = {
-      status: "top"
+      status: "top",
     };
   }
 
-
   componentDidMount() {
-    this.listener = document.addEventListener("scroll", e => {
+    this.listener = document.addEventListener("scroll", (e) => {
       var scrolled = document.scrollingElement.scrollTop;
       if (scrolled >= 120) {
         if (this.state.status !== "bgChanged") {
@@ -45,8 +43,6 @@ export default class HomeHero extends Component {
   }
 
   render() {
-
-
     return (
       <div className="inner">
         {/* <div className="cta-nav-btn">
@@ -55,55 +51,52 @@ export default class HomeHero extends Component {
           </a>
         </div> */}
 
-        <Row>
+        <Row className="hero-row">
           {/* <Col md={3}></Col> */}
 
-          <Col sm={12} md={12} className="home-img" style={{
-            backgroundImage: `url(${heroImg})`,
-            backgroundColor: `#F9F8F0`,
-            opacity: `90%`,
-            backgroundBlendMode: `multiply`,
-            backgroundPosition: `center`,
-            backgroundSize: `cover`,
-            backgroundRepeat: `no-repeat`,
-            backgroundAttachment: `fixed`,
-            height: `${this.props.height}`,
-            // maxHeight: '-webkit-fill-available',
-            display: `flex`,
-            alignItems: `center`,
-            position: `relative`
-            //   cover no-repeat center center fixed`
-          }}>
+          <Col
+            sm={12}
+            md={12}
+            className="home-img"
+            style={{
+              backgroundImage: `url(${heroImg})`,
+              backgroundColor: `#F9F8F0`,
+              opacity: `90%`,
+              backgroundBlendMode: `multiply`,
+              backgroundPosition: `center`,
+              backgroundSize: `cover`,
+              backgroundRepeat: `no-repeat`,
+              backgroundAttachment: `fixed`,
+              height: `${this.props.height}`,
+              // maxHeight: '-webkit-fill-available',
+              display: `flex`,
+              alignItems: `center`,
+              position: `relative`,
+              //   cover no-repeat center center fixed`
+            }}
+          >
             <Image className="hero-logo" src={logo}></Image>
-            {/* <div className="hero-text-block">
-                <h4 className="title-name">Freshness,</h4>
-                <h1 className="title-phrase">Like Never Before</h1>
-            </div> */}
-            {/* <div className="hero-text-block">
-                <h4 className="title-name">Freshness,</h4>
-                <h1 className="title-phrase">Like Never Before</h1>
-            </div> */}
-
-            <br></br>
-            {/* <div> */}
-            {/* <Button className="action-button" variant="outline-dark">Coming Soon     </Button> */}
-            {/* <p className="action-button primaryTextColor">Coming Soon</p> */}
-            {/* <div className="arrow-container">
-            <Image className="arrow-img center" src={arrow}></Image>
-            </div> */}
-            {/* </div> */}
-
-
-
-
-
-
-
+            <div className="hero-content">
+              <div className="hero-hours-block">
+                <img className="hero-icon" src={clock}></img>
+                <div className="hours-details">
+                  <p className="hours-title">Hours</p>
+                  <p className="hours">
+                    MON-SUN <span>| 7am - 7pm</span>
+                  </p>
+                </div>
+              </div>
+              <div className="directions-block">
+                <img className="hero-icon" src={flag}></img>
+                <div className="hours-details">
+                  <p className="directions-title">Directions</p>
+                  <p className="address">1818 Oberlin Rd #103</p>
+                </div>
+              </div>
+            </div>
           </Col>
-          {/* <Col md={3}></Col> */}
         </Row>
       </div>
-    )
-
+    );
   }
 }
