a25f803632167acce9aadd15892a96619b229d44
timsteeleallen <timsteeleallen@gmail.com>
2020-09-11 15:12:29 -0400

    adding margin between icons

--- client/src/components/Brand/index.js
+++ client/src/components/Brand/index.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 // import logo from './logo.svg';
 // import HomeHero from "../../components/HomeHero/index"
 import "./style.css";
-import idleIcons from "../../media/idleicons.png"
+import idleIcons from "../../media/idleicons.png";
 import dots from "../../media/dots.png";
 import clock from "../../media/clock-icon-white.png";
 import cup from "../../media/cup-icon-white.png";
@@ -21,40 +21,42 @@ export default class Brand extends Component {
     this.listener = null;
     this.state = {
       status: "top",
-      icons: [],
+      icons: []
     };
   }
 
   componentDidMount() {
     const icons = [
-      { icon: star,
-        name: 'Star',
-        description:'The beacon of mid-century charm'
-       },
-      { icon: cup ,
-        name: 'Coffee cup',
-        description:'The vessel of our business'
-       },
-      { icon: clock,
-        name: 'Clock',
-        description:'No matter what time, coffee can always be had'
-       },
-      { icon: tamp,
-        name: 'Tamp',
-        description:'The baristas tool of choice'
-       },
-      { icon: latte,
-        name: 'Latte art',
-        description:'The signature of a well crafted beverage'
-       },
-      { icon: flag,
-        name: 'Vietnamese flag',
-        description:'A symbol of our heritage'
-       }
+      {
+        icon: star,
+        name: "Star",
+        description: "The beacon of mid-century charm"
+      },
+      {
+        icon: cup,
+        name: "Coffee cup",
+        description: "The vessel of our business"
+      },
+      {
+        icon: clock,
+        name: "Clock",
+        description: "No matter what time, coffee can always be had"
+      },
+      { icon: tamp, name: "Tamp", description: "The baristas tool of choice" },
+      {
+        icon: latte,
+        name: "Latte art",
+        description: "The signature of a well crafted beverage"
+      },
+      {
+        icon: flag,
+        name: "Vietnamese flag",
+        description: "A symbol of our heritage"
+      }
     ];
 
     this.setState({
-      icons: icons,
+      icons: icons
     });
   }
 
@@ -76,7 +78,7 @@ export default class Brand extends Component {
           backgroundColor: `black`,
           zIndex: "2000",
           // position: `fixed`,
-          width: `100%`,
+          width: `100%`
           // height: `100vh`,
         }}
         className="brand-page"
@@ -84,15 +86,13 @@ export default class Brand extends Component {
         <div className="brand-container">
           <img className="brand-main-icon" width="195px" src={idleIcons}></img>
           <p>Get to know the brand</p>
-            <div className="brand-icon-box">{showIcons}</div>
-         
-          <p className="brand-justsoup">Designed by Just Soup</p>
+          <div className="brand-icon-box">{showIcons}</div>
           <div onClick={this.props.hideBrand} className="close-brand">
-          <p>Back to home</p>
-          <img className="dots-img-brand" src={dots}></img>
-          </div>
+            <p>Back to home</p>
+            <img className="dots-img-brand" src={dots}></img>
           </div>
         </div>
+      </div>
     );
   }
 }
