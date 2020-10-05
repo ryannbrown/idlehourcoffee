5cdc2106927c2deef7495b2572ba6639a1c00355
timsteeleallen <timsteeleallen@gmail.com>
2020-10-05 11:28:41 -0400

    add new menu verbiage

--- client/src/components/MenuHero/index.js
+++ client/src/components/MenuHero/index.js
@@ -7,7 +7,7 @@ import "./style.css";
 // import profPic from "../../media/5.PNG"
 import logo from "../../media/ihlogo.png";
 import blackStar from "../../media/blackstar.png";
-import heroImg from '../../media/shop1.jpg'
+import heroImg from "../../media/shop1.jpg";
 import clock from "../../media/blackclock.png";
 import flag from "../../media/blackflag.png";
 import dots from "../../media/dots.png";
@@ -25,9 +25,9 @@ export default class MenuHero extends Component {
     };
   }
 
-  navTo = (link) => { 
-    window.location.href = '/menu'
-   }
+  navTo = link => {
+    window.location.href = "/menu";
+  };
 
   componentDidMount() {
     this.listener = document.addEventListener("scroll", e => {
@@ -50,33 +50,35 @@ export default class MenuHero extends Component {
 
   render() {
     return (
-        <div className="menu-hero-comp-parent">
+      <div className="menu-hero-comp-parent">
         {/* <img className="hd-comp-dots" src={dots}></img> */}
-      <div onClick={this.navTo} className="menu-hero-comp"
-            style={{
-                backgroundImage: `url(${heroImg})`,
-                // backgroundColor: `#F9F8F0`,
-                opacity: `100%`,
-                backgroundBlendMode: `multiply`,
-                backgroundPosition: `center`,
-                backgroundSize: `cover`,
-                backgroundRepeat: `no-repeat`,
-                // backgroundAttachment: `fixed`,
-                height: `100vh`,
-                position:'relative',
-                // maxHeight: '-webkit-fill-available',
-                  display: `flex`,
-                  alignItems: `center`,
-                  position: `relative`,
-                  justifyContent:'center',
-                color:'white',
-                cursor:'pointer'
-                //   cover no-repeat center center fixed`
-            }}
-            >
-            <h1>Full Menu</h1>
-      </div>
+        <div
+          onClick={this.navTo}
+          className="menu-hero-comp"
+          style={{
+            backgroundImage: `url(${heroImg})`,
+            // backgroundColor: `#F9F8F0`,
+            opacity: `100%`,
+            backgroundBlendMode: `multiply`,
+            backgroundPosition: `center`,
+            backgroundSize: `cover`,
+            backgroundRepeat: `no-repeat`,
+            // backgroundAttachment: `fixed`,
+            height: `100vh`,
+            position: "relative",
+            // maxHeight: '-webkit-fill-available',
+            display: `flex`,
+            alignItems: `center`,
+            position: `relative`,
+            justifyContent: "center",
+            color: "white",
+            cursor: "pointer"
+            //   cover no-repeat center center fixed`
+          }}
+        >
+          <h1>View full menu</h1>
         </div>
+      </div>
     );
   }
 }
